9916156c418f494d9824b0c9f79def823f8b0e2d
mr-spacemango <mca.2301@unigoa.ac.in>
2024-07-26 11:18:44 +0530

    Update Pattern.java agaunb

--- Pattern.java
+++ Pattern.java
@@ -7,7 +7,7 @@ class Pattern {
     public static void main(String args[])
     {
         System.out.println("Hello, World");
-        
+        //as
         System.out.println("Hello, Umang");
     
     }
